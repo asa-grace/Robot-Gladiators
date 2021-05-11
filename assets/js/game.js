fb880163349a82f525de957d0410e2d0870b733e
Asa Grace <asag182@gmail.com>
2021-05-11 14:02:35 -0600

    add HTML with script, add js

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
+var playerName = "Tony Robo";
+console.log(playerName)
+    
+console.log("this logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+console.log(10+10);
+
+console.log("Our robot's name is " + playerName);
+
+//fight();
